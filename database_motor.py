b4983be7f5a4fa5f4059c776403f2d13931e333a
preacher6 <juafelgrajalesg@gmail.com>
2020-11-01 19:41:07 -0500

    Consultas agregadas

--- database_motor.py
+++ database_motor.py
@@ -1,3 +1,15 @@
+
 from pymongo import MongoClient
 
 client = MongoClient('localhost')
+
+database_ = client['prueba']
+
+coleccion = database_['usuarios']
+
+""" coleccion.insert_many([{'edad':14, 'nombre':'ajuli', 'interes': ['viejas']},
+                        {'edad':65, 'nombre':'olo', 'interes': ['hockey']},
+                        {'edad':23, 'nombre':'arty', 'interes': ['computo']}]) """
+
+for documento in coleccion.find({'edad':{"$gt":14}}):
+    print(documento)
