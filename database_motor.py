ef731d0f3a6de6c9935fb875c17153eadfd0003e
preacher6 <juafelgrajalesg@gmail.com>
2020-11-01 19:55:34 -0500

    Funcional

--- database_motor.py
+++ database_motor.py
@@ -1,5 +1,4 @@
-
-from pymongo import MongoClient
+from pymongo import MongoClient, ASCENDING, DESCENDING
 
 client = MongoClient('localhost')
 
@@ -7,9 +6,28 @@ database_ = client['prueba']
 
 coleccion = database_['usuarios']
 
+# Insertar varios elementos
 """ coleccion.insert_many([{'edad':14, 'nombre':'ajuli', 'interes': ['viejas']},
                         {'edad':65, 'nombre':'olo', 'interes': ['hockey']},
-                        {'edad':23, 'nombre':'arty', 'interes': ['computo']}]) """
+                        {'edad':23, 'nombre':'marty', 'interes': ['computo']}]) """
 
-for documento in coleccion.find({'edad':{"$gt":14}}):
+# Realizar consultas 
+for documento in coleccion.find({'edad':{"$gte":10}}):
     print(documento)
+
+#print(coleccion.find_one({'edad':{"$gt":24}}))
+
+# Eliminar elementos
+""" coleccion.delete_many({'edad':{"$lte":20}})
+coleccion.delete_one({'edad': {'$gte':65}}) """
+
+# Actualizar elementos
+#coleccion.update_one({"edad":{"$lt":40}}, {"$set":{"edad":50}})
+#coleccion.create_index([('edad', ASCENDING)])
+
+# Eliminar coleccion
+#database_.drop_collection('usuarios')
+
+# Eliminar base de datos
+client.drop_database('prueba')
+print(client.list_database_names())
